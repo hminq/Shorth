f3ac2fa3630d26372a972d36acb88057ce72d5f5
hminh <hminh250104@gmail.com>
2026-05-09 14:31:52 +0700

    feat: add br const

---
+++ backend/Domain/Constants/SlugRules.cs
@@ -0,0 +1,7 @@
+namespace Domain.Constants;
+
+public static class SlugRules
+{
+    public const int SlugLength = 6;
+    public const string SlugCharacterSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+}
