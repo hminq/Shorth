a5944e1e32f5e2323935a8675192f41fbe37ac93
hminh <hminh250104@gmail.com>
2026-05-09 14:39:12 +0700

    feat: add more env config

--- backend/Infrastucture/DependencyInjection.cs
+++ backend/Infrastucture/DependencyInjection.cs
@@ -1,5 +1,8 @@
+using Application.Abstractions;
 using Infrastucture.Database;
+using Infrastucture.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,14 +24,38 @@ public static class DependencyInjection
             throw new InvalidOperationException("Redis connection string is not configured.");
         }
 
+        var redisInstanceName = configuration["Redis:InstanceName"];
+        if (string.IsNullOrWhiteSpace(redisInstanceName))
+        {
+            throw new InvalidOperationException("Redis instance name is not configured.");
+        }
+
+        var redisLinkTtlHour = configuration["Redis:LinkDestinationUrlTtlHours"];
+        if (string.IsNullOrWhiteSpace(redisLinkTtlHour))
+        {
+            throw new InvalidOperationException("Redis link ttl is not configured.");
+        }
+
+        if (!int.TryParse(redisLinkTtlHour, out var redisLinkTtlHours) || redisLinkTtlHours <= 0)
+        {
+            throw new InvalidOperationException("Redis link ttl hours must be a valid positive integer.");
+        }
+
         services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(connectionString));
 
         services.AddStackExchangeRedisCache(options =>
         {
             options.Configuration = redisConnectionString;
-            options.InstanceName = "shorth:";
+            options.InstanceName = redisInstanceName;
         });
+
+        services.AddScoped<ILinkRepository, LinkRepository>();
+        services.AddScoped<ILinkCacheRepository>(_ =>
+            new LinkCacheRepository(
+                _.GetRequiredService<IDistributedCache>(),
+                TimeSpan.FromHours(redisLinkTtlHours)));
+        services.AddScoped<ISlugGenerator, SlugGenerator>();
 
         return services;
     }
