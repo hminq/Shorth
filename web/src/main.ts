a41099af3c3cc5128313c4a5fd617a6ac64fb427
hminh <hminh250104@gmail.com>
2026-05-09 14:29:03 +0700

    small changes

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <main class="shell">
-  <img class="brandmark" src="/shorth_logo.png" alt="Shorth logo" />
+  <img class="brandmark" src="/shorth_logo.svg" alt="Shorth logo" />
   <p class="eyebrow">URL Shortener</p>
   <h1>Shorth</h1>
   <p class="lead">Fast link creation, clean redirects, and room for analytics when you need them.</p>
