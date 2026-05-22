89467088543c56fd54f5500431b1982510e382c3
hminh <hminh250104@gmail.com>
2026-05-22 10:02:47 +0700

    increase target load in k6 redirect performance test

--- test/redirect-hot-path.js
+++ test/redirect-hot-path.js
@@ -27,9 +27,9 @@ if (!slug) {
 export const options = {
   maxRedirects: 0,
   stages: [
-    { duration: '20s', target: 75 },
-    { duration: '40s', target: 150 },
-    { duration: '40s', target: 300 },
+    { duration: '20s', target: 100 },
+    { duration: '40s', target: 200 },
+    { duration: '40s', target: 400 },
     { duration: '20s', target: 0 }
   ],
   thresholds: {
